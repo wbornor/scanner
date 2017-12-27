465ececab976f37f89ccaf2cfeeb2e721f0e92a3
211016759 <william.bornor@geappliances.com>
2017-12-27 08:09:47 -0500

    setuptools

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='scanner',
+      version='0.0.0',
+      description='',
+      url='https://github.com/wbornor/scanner',
+      author='wbornor',
+      author_email='wbornor+github@gmail.com',
+      packages=[],
+      install_requires=[
+          'numpy',
+      ],
+      dependency_links=['https://github.com/sightmachine/SimpleCV'],
+      zip_safe=False)
