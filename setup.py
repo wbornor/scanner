19d68b5614e23bef697663678c23f1e355bd89dd
211016759 <william.bornor@geappliances.com>
2017-12-27 08:13:41 -0500

    more dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,10 @@ setup(name='scanner',
       author_email='wbornor+github@gmail.com',
       packages=[],
       install_requires=[
-          'numpy',
+            'numpy',
+            'SimpleCV',
+            'PiCamera',
+            'zbar-py'
       ],
       dependency_links=['https://github.com/sightmachine/SimpleCV'],
       zip_safe=False)
