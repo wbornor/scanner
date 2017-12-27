8a47b0d8900f7b4af65b54917d4187fbc757d804
211016759 <william.bornor@geappliances.com>
2017-12-26 22:29:32 -0500

    glob handling

--- scanner.py
+++ scanner.py
@@ -2,14 +2,19 @@ import cv2
 import zbar.misc
 import sys
 
-file = sys.argv[1]
+if len(sys.argv) < 2:
+    print 'usage: scanner <file>'
 
-image = cv2.imread(file)
-print(type(image))
-if len(image.shape) == 3:
-    image = zbar.misc.rgb2gray(image)
-scanner = zbar.Scanner()
-results = scanner.scan(image)
-for result in results:
-    if result.type == 'UPC-A':
-        print(result.data, zbar.misc.upca_is_valid(result.data.decode('ascii')), result.quality)
+for file in sys.argv:
+    if "scanner.py" in file:
+        continue
+
+    image = cv2.imread(file)
+    print(type(image))
+    if len(image.shape) == 3:
+        image = zbar.misc.rgb2gray(image)
+    scanner = zbar.Scanner()
+    results = scanner.scan(image)
+    for result in results:
+        if result.type == 'UPC-A':
+            print(result.data, zbar.misc.upca_is_valid(result.data.decode('ascii')), result.quality, file)
