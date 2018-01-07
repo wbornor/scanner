37840cc5aa64d0c54d806ab2b3ca92a537d9d63b
211016759 <william.bornor@geappliances.com>
2018-01-06 21:57:20 -0500

    apply buzzer code

--- test-chirp.py
+++ test-chirp.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python3
 
-from time import sleep
+import time
 import RPi.GPIO as GPIO
 
+__pin__ = 22
 
-def chirp():
-    GPIO.output(22, GPIO.HIGH)
-    sleep(0.2)
-    GPIO.output(22, GPIO.LOW)
+
+def buzz(pitch, duration):  # create the function “buzz” and feed it the pitch and duration)
+
+    if (pitch == 0):
+        time.sleep(duration)
+        return
+    period = 1.0 / pitch  # in physics, the period (sec/cyc) is the inverse of the frequency (cyc/sec)
+    delay = period / 2  # calcuate the time for half of the wave
+    cycles = int(duration * pitch)  # the number of waves to produce is the duration times the frequency
+
+    for i in range(cycles):  # start a loop from 0 to the variable “cycles” calculated above
+        GPIO.output(__pin__, GPIO.HIGH)  # set pin 18 to high
+        time.sleep(delay)  # wait with pin 18 high
+        GPIO.output(__pin__, GPIO.LOW)  # set pin 18 to low
+        time.sleep(delay)  # wait with pin 18 low
 
 
 def main():
     # set up GPIO output channel
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(22, GPIO.IN)
-    GPIO.setup(22, GPIO.OUT)
+    GPIO.setup(__pin__, GPIO.IN)
+    GPIO.setup(__pin__, GPIO.OUT)
 
     while True:
-        chirp()
+        buzz(500, 0.1)
+        time.sleep(0.5)
 
 
 if __name__ == '__main__':
